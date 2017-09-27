0a1dc6bb22822d5b6ce9e0d04ffdfd217fd9965b
st970703 <st970703@gmail.com>
2017-09-28 12:57:49 +1300

    q2

--- a2fuse1.py
+++ a2fuse1.py
@@ -1,31 +1,18 @@
 
 #!/usr/bin/env python
 
-
 from __future__ import print_function, absolute_import, division
 
 import logging
-import sys
 
-from fuse import FUSE
+import os
+import sys
+import errno
+
+from fuse import FUSE, FuseOSError, Operations, LoggingMixIn
 from passthrough import Passthrough
 
-
-class LoggingMixIn:
-    log = logging.getLogger('A2')
-
-    def __call__(self, op, path, *args):
-        self.log.debug('-> path:%s %s%s', path, op, repr(args))
-        ret = '[Unhandled Exception]'
-        try:
-            ret = getattr(self, op)(path, *args)
-            return ret
-        except OSError as e:
-            ret = str(e)
-            raise
-
 class A2Fuse1(LoggingMixIn, Passthrough):
-
 	pass
 
 def main(mountpoint, root):
