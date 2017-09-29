c4b46a61292d378e95addd8cc9427684cc256f3e
st970703 <st970703@gmail.com>
2017-09-29 16:28:32 +1300

    add memory part

--- a2fuse2.py
+++ a2fuse2.py
@@ -8,11 +8,51 @@ import os
 import sys
 import errno
 
+import memory
+
 from fuse import FUSE, FuseOSError, Operations, LoggingMixIn
 from passthrough import Passthrough
 
+# elee353, 840454023
 class A2Fuse2(LoggingMixIn, Passthrough):
-    pass
+
+    def __init__(self, root):
+        super(self, root)
+        self.memory = memory()
+
+    def getattr(self, path, fh=None):
+        # in memory
+        if path not in self.files:
+            full_path = self._full_path(path)
+            st = os.lstat(full_path)
+            return dict((key, getattr(st, key)) for key in ('st_atime', 'st_ctime',
+                                                            'st_gid', 'st_mode', 'st_mtime', 'st_nlink', 'st_size',
+                                                            'st_uid'))
+        # in user space
+        else:
+            return self.files[path]
+
+    def readdir(self, path, fh):
+        pass
+
+    def open(self, path, flags):
+        pass
+
+    def create(self, path, mode, fi=None):
+        pass
+
+    def unlink(self, path):
+        pass
+
+    def write(self, path, buf, offset, fh):
+        pass
+
+    def read(self, path, length, offset, fh):
+        pass
+
+        # __init__, getattr, readdir
+        # open, create, unlink
+        # write, read
 
 def main(mountpoint, root):
     FUSE(A2Fuse2(root), mountpoint, nothreads=True, foreground=True)
@@ -20,38 +60,3 @@ def main(mountpoint, root):
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
     main(sys.argv[2], sys.argv[1])
-
-    # __init__, getattr, readdir
-    # open, create, unlink
-    # write, read
-
-def __init__(self):
-    pass
-
-def getattr(self, path, fh=None):
-    if path not in self.files:
-        full_path = self._full_path(path)
-        st = os.lstat(full_path)
-        return dict((key, getattr(st, key)) for key in ('st_atime', 'st_ctime',
-                                                        'st_gid', 'st_mode', 'st_mtime', 'st_nlink', 'st_size',
-                                                        'st_uid'))
-    else:
-        return self.files[path]
-
-def readdir(self, path, fh):
-    pass
-
-def open(self, path, flags):
-    pass
-
-def create(self, path, mode, fi=None):
-    pass
-
-def unlink(self, path):
-    pass
-
-def write(self, path, buf, offset, fh):
-    pass
-
-def read(self, path, length, offset, fh):
-    pass
